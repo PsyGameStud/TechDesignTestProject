bd617de93fd100d975ac0a623e0a8afbbfdbd797
Artur Kozlov <psygamestud@gmail.com>
2023-09-09 17:21:52 +0300

    add: added localization

--- Assets/Scripts/MainMenu/MainMenuEntryPoint.cs
+++ Assets/Scripts/MainMenu/MainMenuEntryPoint.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using Lean.Localization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,11 +8,17 @@ public class MainMenuEntryPoint : MonoBehaviour
 {
     [SerializeField] private Button _buttonPlay;
     [SerializeField] private Button _buttonExitGame;
+    [SerializeField] private Button _buttonLanguage;
+    [SerializeField] private LeanLocalization _leanLocalization;
+
+    private const string _ru = "russian";
+    private const string _eng = "english";
     
     private void Start()
     {
         _buttonPlay.onClick.AddListener(() => { SceneManager.LoadScene("scene_game"); });
         _buttonExitGame.onClick.AddListener(Application.Quit);
+        _buttonLanguage.onClick.AddListener(ChangeLanguage);
         
         var buttonPlayTransform = _buttonPlay.transform;
         buttonPlayTransform.localScale = Vector3.zero;
@@ -22,5 +29,19 @@ public class MainMenuEntryPoint : MonoBehaviour
         buttonExitTransform.localScale = Vector3.zero;
 
         buttonExitTransform.DOScale(Vector3.one, 1f).SetEase(Ease.OutBack);
+    }
+
+    private void ChangeLanguage()
+    {
+        if (_leanLocalization.CurrentLanguage == _ru)
+        {
+            _leanLocalization.CurrentLanguage = _eng;
+            return;
+        }
+        
+        if (_leanLocalization.CurrentLanguage == _eng)
+        {
+            _leanLocalization.CurrentLanguage = _ru;
+        }
     }
 }
